43bb07b8dff969d00b24411a66272e5debe040db
Random520 <119103538+Random520@users.noreply.github.com>
2024-06-09 13:12:29 +0530

    Updated app.py ai

--- app.py
+++ app.py
@@ -1,24 +1,31 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_socketio import SocketIO, join_room, leave_room
+import pyttsx3
+import time
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
 
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+  
 app = Flask(__name__)
-socketio = SocketIO(app)
+
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
 
-@app.route('/chat')
-def chat():
-    username = request.args.get('username')
-    room = request.args.get('room')
-
-    if username and room:
-        return render_template('chat.html', username=username, room=room)
-    else:
-        return redirect(url_for('home'))
-
-
-if __name__ == '__main__':
-    socketio.run(app, debug=True)w
+@app.route('/AI', methods=['GET', 'POST']) 
+def says():
+    if request.method == 'POST': 
+    
+        word = request.form.get('say') 
+        return render_template("ex.html", word=word )
+        time.sleep(5) 
+        speak("Hello")
+    else: 
+         
+     return render_template("index.html") 
+app.run        
