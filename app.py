0ecfb196e32814d1cb6f002a555fd27467a6c03e
Random520 <119103538+Random520@users.noreply.github.com>
2024-10-04 17:15:11 +0530

    Update app.py
    Signed-off-by: Random520 <119103538+Random520@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,13 +1,4 @@
 from flask import Flask, render_template, request, redirect, url_for
-import pyttsx3
-import time
-engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
-
-def speak(audio):
-    engine.say(audio)
-    engine.runAndWait()
   
 app = Flask(__name__)
 
@@ -17,15 +8,7 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/AI', methods=['GET', 'POST']) 
-def says():
-    if request.method == 'POST': 
-    
-        word = request.form.get('say') 
-        return render_template("ex.html", word=word )
-        time.sleep(2) 
-        speak("word")
-    else: 
-         
-     return render_template("index.html") 
+@app.route('/AI') 
+def says():    
+        return render_template("ex.html", word=word )  
 app.run        
